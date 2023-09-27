9ab23e5bfb03613af32a696fd1873055eda9d7b2
BirdMouth <birdinsideofamouth@gmail.com>
2023-09-27 18:00:23 +0300

    Ask user for city name
    Added simple functions to request user input for a city name and validating
    that the given input doesn't contain numbers.

--- weather_app/src/main.rs
+++ weather_app/src/main.rs
@@ -1,3 +1,33 @@
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    let mut city_name: String = String::new();
+
+    ask_city_name(&mut city_name);
+}
+
+fn ask_city_name(city_name: &mut String) {
+    loop {
+        println!("Enter a city name: ");
+
+        stdin().read_line(city_name).unwrap();
+        city_name.pop();
+
+        let valid_input = is_string_alphabetic(city_name.to_string());
+        if valid_input {
+            break;
+        }
+
+        println!("Invalid city name '{}'!\n", city_name);
+        city_name.clear();
+    }
+}
+
+fn is_string_alphabetic(check_str: String) -> bool {
+    for c in check_str.chars() {
+        if c.is_numeric() {
+            return false;
+        }
+    }
+    return true;
 }
