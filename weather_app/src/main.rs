d3c6f9f8d4ee49e871e46c0afde3f5e809623c46
BirdMouth <birdinsideofamouth@gmail.com>
2023-09-27 18:49:23 +0300

    Add HTTP GET for city details
    Added HTTP GET request for city details based on user provided city name.
    HTTP GET is made to open-meteo API.

--- weather_app/src/main.rs
+++ weather_app/src/main.rs
@@ -1,9 +1,23 @@
 use std::io::stdin;
 
-fn main() {
-    let mut city_name: String = String::new();
+static CITY_NAME_PLACEHOLDER: &str = "[CITY_NAME]";
 
+fn main() {
+    let api_url: String = format!("{}{}{}", "https://geocoding-api.open-meteo.com/v1/search?name=",
+                                            CITY_NAME_PLACEHOLDER,
+                                            "&count=1&language=en&format=json");
+
+    let mut city_name: String = String::new();
     ask_city_name(&mut city_name);
+
+    let request_url: String = api_url.to_string().replace(CITY_NAME_PLACEHOLDER, &city_name.to_owned());
+    let city_details = reqwest::blocking::get(request_url).unwrap().text();
+
+    println!("{:#?}", city_details);
+
+    // Todo:
+    // Parse coordinates from city_details and use them to request city weather data from:
+    // https://open-meteo.com/en/docs#latitude=65.0124&longitude=25.4682
 }
 
 fn ask_city_name(city_name: &mut String) {
