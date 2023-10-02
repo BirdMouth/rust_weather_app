fe22293171bcd9ad3a7e246fe17b85c4a3fbaa11
BirdMouth <birdinsideofamouth@gmail.com>
2023-10-02 18:57:58 +0300

    Add current weather println
    Added printing out the current weather and refactored city details
    API call.

--- weather_app/src/main.rs
+++ weather_app/src/main.rs
@@ -1,7 +1,5 @@
-use std::io::stdin;
 use serde_json::Value;
-
-static CITY_NAME_PLACEHOLDER: &str = "[CITY_NAME]";
+use std::io::stdin;
 
 fn main() {
     let mut city_name: String = String::new();
@@ -9,10 +7,10 @@ fn main() {
     ask_city_name(&mut city_name);
     let city_details_json: Value = get_city_details_as_json(&mut city_name);
     let city_lat_lon: (String, String) = get_city_lat_lon(&city_details_json);
-
-    // TODO:
-    // Check that city_lat_lon contains valid values (not null).
-    // Send a request to https://api.open-meteo.com/v1/ for weather forecast using city_lat_lon as parameters
+    let weather_details: Value = get_city_weather_from_lat_lon(city_lat_lon.0, city_lat_lon.1);
+    println!("It is {} celsius in {}, WMO code is {}.", weather_details["current_weather"]["temperature"],
+                                                        city_name,
+                                                        weather_details["current_weather"]["weathercode"]);
 }
 
 fn ask_city_name(city_name: &mut String) {
@@ -34,11 +32,10 @@ fn ask_city_name(city_name: &mut String) {
 
 fn get_city_details_as_json(city_name: &mut String) -> Value {
     let api_url: String = format!("{}{}{}", "https://geocoding-api.open-meteo.com/v1/search?name=",
-                                            CITY_NAME_PLACEHOLDER,
+                                            city_name,
                                             "&count=1&language=en&format=json");
 
-    let request_url: String = api_url.to_string().replace(CITY_NAME_PLACEHOLDER, &city_name.to_owned());
-    let city_details: String = reqwest::blocking::get(request_url).unwrap().text().expect("Failed to get payload");
+    let city_details: String = reqwest::blocking::get(api_url).unwrap().text().expect("Failed to get payload");
     let city_details_json: Value =  serde_json::from_str(city_details.as_str()).unwrap();
     return city_details_json;
 }
@@ -47,6 +44,17 @@ fn get_city_lat_lon(city_details_json: &Value) -> (String, String) {
     let city_lat: String = city_details_json["results"][0]["latitude"].to_string();
     let city_lon: String = city_details_json["results"][0]["longitude"].to_string();
     return (city_lat, city_lon);
+}
+
+fn get_city_weather_from_lat_lon(lat: String, lon: String) -> Value {
+    let request_url: String = format!("{}{}{}{}{}", "https://api.open-meteo.com/v1/forecast?latitude=",
+                                                    lat,
+                                                    "&longitude=",
+                                                    lon,
+                                                    "&current_weather=true");
+    let weather: String = reqwest::blocking::get(request_url).unwrap().text().expect("Failed to get payload");
+    let weather_json: Value =  serde_json::from_str(weather.as_str()).unwrap();
+    return weather_json;
 }
 
 fn is_string_alphabetic(check_str: String) -> bool {
